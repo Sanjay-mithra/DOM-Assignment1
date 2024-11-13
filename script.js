e89e7a9d17d9873d7caec9e74c263cc2379ead0b
Sanjay mithra <sanjaymithra01@gmail.com>
2024-11-13 23:50:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let title=document.getElementById("main-title")
+console.log(title);
+
+let div=document.getElementsByClassName("container")
+console.log(div);
+
+let section=document.getElementsByClassName("section")
+console.log(section);
+
+let paragraph=document.getElementsByTagName("p")
+console.log(paragraph);
+
+let button=document.querySelector(".btn")
+console.log(button);
+
+let section1=document.querySelector(".section")
+console.log(section1);
+
+let buttons=document.querySelectorAll(".btn")
+console.log(buttons);
+
+let paragraphs=document.querySelectorAll("p")
+console.log(paragraphs);
